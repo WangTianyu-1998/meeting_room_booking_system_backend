3564bc0ae594fd21d26820eecc5911615249dae2
王添瑜 <11081572+tcwty123@user.noreply.gitee.com>
2025-02-23 21:55:38 +0800

    feat: 用户修改信息等接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,10 +11,13 @@ import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { RedisService } from 'src/redis/redis.service';
 import { EmailService } from 'src/email/email.service';
-import { ResponseUtil } from 'src/utils';
 import { LoginUserDto } from './dto/login-user.dto';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { RequireLogin, UserInfo } from 'src/custom.decorator';
+import { UserDetailVo } from './vo/user-info.vo';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -44,7 +47,7 @@ export class UserController {
     return await this.userService.register(registerUser);
   }
 
-  // 发送邮件
+  // 发送邮件-注册
   @Get('register-captcha')
   async captcha(@Query('address') address: string) {
     const code = Math.random().toString().slice(2, 8);
@@ -195,5 +198,100 @@ export class UserController {
     } catch (e) {
       throw new UnauthorizedException('token 已失效，请重新登录');
     }
+  }
+
+  /**
+   * 获取用户详情
+   * @param userId
+   * @returns
+   */
+  @Get('info')
+  @RequireLogin()
+  async info(@UserInfo('userId') userId: number) {
+    const user = await this.userService.findUserDetailById(userId);
+    const vo = new UserDetailVo();
+    vo.id = user!.id;
+    vo.email = user!.email;
+    vo.username = user!.username;
+    vo.headPic = user!.headPic;
+    vo.phoneNumber = user!.phoneNumber;
+    vo.nickName = user!.nickName;
+    vo.isFrozen = user!.isFrozen;
+    vo.createTime = user!.createTime;
+
+    return vo;
+  }
+
+  /**
+   * 修改用户密码
+   * @param userId
+   * @param passwordDto
+   * @returns
+   */
+  @Post(['update_password', 'admin/update_password'])
+  @RequireLogin()
+  updatePassword(
+    @UserInfo('userId') userId: number,
+    @Body() passwordDto: UpdateUserPasswordDto,
+  ) {
+    console.log('userId----', userId);
+    return this.userService.updatePassword(userId, passwordDto);
+  }
+
+  /**
+   * 获取邮箱验证码密码
+   */
+  @Get('update_password/captcha')
+  async updatePasswordCaptcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+    await this.redisService.set(
+      `update_password_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改密码验证码',
+      html: `<p style="font-size:24;color: orangered; font-weight: 700;">欢迎来到瑜小窝, 你的注册验证码是 ${code}</p>`,
+    });
+    return '发送成功';
+  }
+
+  /**
+   * 更新用户信息
+   * @param userId
+   * @param updateUserDto
+   * @returns
+   */
+  @Post(['update', 'admin/update'])
+  @RequireLogin()
+  async update(
+    @UserInfo('userId') userId: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.userService.update(userId, updateUserDto);
+  }
+
+  /**
+   * 获取更新用户信息的邮箱验证码
+   * @param address
+   * @returns
+   */
+  @Get('update/captcha')
+  async updateCaptcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_user_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改用户信息验证码',
+      html: `<p>你的验证码是 ${code}</p>`,
+    });
+    return '发送成功';
   }
 }
