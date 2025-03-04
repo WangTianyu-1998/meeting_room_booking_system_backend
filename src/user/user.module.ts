0ede2be309fb7054893a18acae0f1a263678567a
王添瑜 <11081572+tcwty123@user.noreply.gitee.com>
2025-03-04 23:19:35 +0800

    feat: src

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Role } from '../../src/role/entities/role.entity';
+import { Role } from '../role/entities/role.entity';
 import { Permission } from '../permission/entities/permission.entity';
 
 @Module({
